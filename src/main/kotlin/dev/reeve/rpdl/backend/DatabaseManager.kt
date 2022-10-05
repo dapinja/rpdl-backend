e460328160b38128c8e34e81854c471db9829999
Reeve/Xwy <reeveblake@gmail.com>
2022-10-05 15:36:30 -0400

    Update DatabaseManager.kt

--- src/main/kotlin/dev/reeve/rpdl/backend/DatabaseManager.kt
+++ src/main/kotlin/dev/reeve/rpdl/backend/DatabaseManager.kt
@@ -19,7 +19,7 @@ class DatabaseManager : Closeable {
 		DriverManager.registerDriver(Driver())
 	}
 	
-	private val dotenv = Dotenv.load()
+	private val dotenv = Dotenv.configure().ignoreIfMissing().load()
 	
 	private val connection = when (Settings.databaseType) {
 		Settings.DatabaseType.SQLITE -> DriverManager.getConnection("jdbc:sqlite:${Settings.databasePath}")
