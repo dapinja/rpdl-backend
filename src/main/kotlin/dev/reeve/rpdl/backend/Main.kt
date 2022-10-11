a4f6609ac2c3c4a5eb2e66ace66f2a6e0adf9ab1
Reeve/Xwy <reeveblake@gmail.com>
2022-10-10 21:04:10 -0400

    upd

--- src/main/kotlin/dev/reeve/rpdl/backend/Main.kt
+++ src/main/kotlin/dev/reeve/rpdl/backend/Main.kt
@@ -57,7 +57,7 @@ fun main(args: Array<String>): Unit = runBlocking {
 				
 				get("/getGameInfo") {
 					val gameId = call.request.queryParameters["game"]?.toIntOrNull() ?: return@get call.respondText("Game arg not found", status = HttpStatusCode.NotFound)
-					val info = Settings.databaseManager.getGameInstance(gameId) ?: return@get call.respondText("Game not found", status = HttpStatusCode.NotFound)
+					val info = Settings.databaseManager.getGameInstance(gameId) ?: return@get call.respondText("Game not found: $gameId", status = HttpStatusCode.NotFound)
 					call.respond(info)
 				}
 				
@@ -70,7 +70,7 @@ fun main(args: Array<String>): Unit = runBlocking {
 				
 				get("/getFullGameInfo") {
 					val gameId = call.request.queryParameters["game"]?.toIntOrNull() ?: return@get call.respondText("Game arg not found", status = HttpStatusCode.NotFound)
-					val info = Settings.databaseManager.getExtendedGameInstance(gameId) ?: return@get call.respondText("Game not found", status = HttpStatusCode.NotFound)
+					val info = Settings.databaseManager.getExtendedGameInstance(gameId) ?: return@get call.respondText("Game not found: $gameId", status = HttpStatusCode.NotFound)
 					call.respond(info)
 				}
 				
