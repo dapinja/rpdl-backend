8e0df59972b1c5e681483a88905cd959e6e2a785
Reeve/Xwy <reeveblake@gmail.com>
2022-11-01 19:23:19 -0400

    Update Config.kt

--- src/main/kotlin/dev/reeve/rpdl/backend/Config.kt
+++ src/main/kotlin/dev/reeve/rpdl/backend/Config.kt
@@ -1,7 +1,7 @@
 package dev.reeve.rpdl.backend
 
 class Config {
-	var postgresAddress = "db"
+	var postgresAddress = "rpdl-db"
 	var port = 5432 // on docker -> 5432, on local -> 5672
 	var databasePath = "./data/info.db"
 }
