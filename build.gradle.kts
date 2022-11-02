6d975026ea8b1414dac066c0e1cdea7b17e9a39f
Reeve/Xwy <reeveblake@gmail.com>
2022-11-02 15:33:23 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,15 +21,15 @@ dependencies {
     implementation("com.github.gotson:sqlite-jdbc:3.32.3.8")
     implementation("org.postgresql:postgresql:42.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.10")
     
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     
-    implementation("io.ktor:ktor-server-netty:2.1.2")
-    implementation("io.ktor:ktor-server-core:2.1.2")
+    implementation("io.ktor:ktor-server-netty:2.1.3")
+    implementation("io.ktor:ktor-server-core:2.1.3")
     implementation("io.ktor:ktor-gson:1.6.8")
-    implementation("io.ktor:ktor-serialization-gson:2.1.2")
-    implementation("io.ktor:ktor-server-content-negotiation:2.1.2")
+    implementation("io.ktor:ktor-serialization-gson:2.1.3")
+    implementation("io.ktor:ktor-server-content-negotiation:2.1.3")
     
     implementation("dev.reeve:TorrustApiWrapper:latest")
     
